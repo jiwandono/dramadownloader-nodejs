bb71306c05564b7a8f57a9144c26b53dccb78653
Adityo Jiwandono <jiwandono@gmail.com>
2014-06-24 20:48:20 +0700

    Bugfix issue #1: Multipart download links point only to first part of movie.

--- downloader/dramago.js
+++ downloader/dramago.js
@@ -21,25 +21,30 @@ DownloaderImpl.prototype.getDownloadables = function(url) {
 
 	var $ = cheerio.load(html);
 	var title = $('h1.generic').text().trim();
-	var parts = $($('.part_list')[0]).find('a');
+	
+	var iframes = $('#streams iframe');
+	var compatibleIframeNumber = -1;
+	for(var i = 0; i < iframes.length; i++) {
+		if(iframes[i].attribs.src.indexOf(iframePrefix) === 0) compatibleIframeNumber = i;
+	}
+	if(compatibleIframeNumber < 0) return downloadables;
+	
+	var parts = $($('.part_list')[compatibleIframeNumber]).find('a');
 	for(var j = 0; j < parts.length; j++) {
-		parts[j].attribs.href = parts[j].attribs.href.replace('/1-1', ''); // Workaround because httpsync doesn't follow redirect
+		parts[j].attribs.href = parts[j].attribs.href.replace('/' + (compatibleIframeNumber+1) + '-1', ''); // Workaround because httpsync doesn't follow redirect
 		var partHtml = util.getHtml(parts[j].attribs.href);
-		
 		var $part = cheerio.load(partHtml);
-		var iframes = $part('iframe[src]');
-		for(var i = 0; i < iframes.length; i++) {
-			var src = iframes[i].attribs.src;
-			if(src.indexOf(iframePrefix) === 0) {
-				var iframeHtml = util.getHtml(src);
-				var downloadUrl = util.substring(iframeHtml, linkPrefix, linkSuffix);
-				downloadUrl = decodeURIComponent(downloadUrl);
-				downloadables.push(new Downloadables({
-					url: downloadUrl,
-					title: title,
-					thumbnail: null
-				}));
-			}
+		var iframe = $part('#streams iframe')[compatibleIframeNumber];
+		var src = iframe.attribs.src;
+		if(src.indexOf(iframePrefix) === 0) {
+			var iframeHtml = util.getHtml(src);
+			var downloadUrl = util.substring(iframeHtml, linkPrefix, linkSuffix);
+			downloadUrl = decodeURIComponent(downloadUrl);
+			downloadables.push(new Downloadables({
+				url: downloadUrl,
+				title: title,
+				thumbnail: null
+			}));
 		}
 	}
 
