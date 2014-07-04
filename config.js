6ada4717f9eadc85b7b8c099f854d82299a90285
Adityo Jiwandono <jiwandono@gmail.com>
2014-07-05 01:00:12 +0700

    Remove obsolete gooddrama.js.

--- config.js
+++ config.js
@@ -11,6 +11,5 @@ module.exports.fileSuffix = '[DramaDownloader.com]';
 module.exports.downloaders = [
 	require('./downloader/dramacool'),
 	require('./downloader/dramago'),
-	require('./downloader/gooddrama'),
 	require('./downloader/animehere')
 ];
