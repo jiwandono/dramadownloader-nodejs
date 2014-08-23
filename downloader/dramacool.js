eed34e35143d799c472cd6ae5497d9ec7685dad7
Adityo Jiwandono <jiwandono@gmail.com>
2014-08-23 16:58:46 +0700

    Fix downloading from DramaCool.com.

--- downloader/dramacool.js
+++ downloader/dramacool.js
@@ -20,14 +20,25 @@ DownloaderImpl.prototype.getDownloadables = function(url, callback) {
 		
 		if(downloadUrl) {
 			var title = $('.title-detail-ep-film').text().trim();
-			
 			downloadUrl += '&title=' + util.buildFilename(title);
-
 			downloadables.push(new Downloadables({
 				url: downloadUrl,
 				title: title,
 				thumbnail: null
 			}));
+		} else {
+			// Method 2: Base64Decode part of iframe src.
+			var downloadUrlBase64 = $('iframe[src^="/embeddrama.php"]').attr('src');
+			if(downloadUrlBase64) {
+				var downloadUrl = new Buffer(downloadUrlBase64.substr(19), 'base64').toString('ascii');
+				var title = $('.title-detail-ep-film').text().trim();
+				downloadUrl += '&title=' + util.buildFilename(title);
+				downloadables.push(new Downloadables({
+					url: downloadUrl,
+					title: title,
+					thumbnail: null
+				}));
+			}
 		}
 
 		callback(downloadables);
