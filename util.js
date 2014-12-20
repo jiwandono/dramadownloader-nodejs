7fcaff610b12c96bc095938193eaf6e0fb510e23
Adityo Jiwandono <jiwandono@gmail.com>
2014-12-20 13:06:27 +0700

    Remove cookie from header

--- util.js
+++ util.js
@@ -10,8 +10,7 @@ var requestOptions = {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
 		'Referer': 'https://www.google.com/',
 		//'Accept-Encoding': 'gzip, deflate',
-		'Accept-Language': 'en-US,en;q=0.8',
-		'Cookie': 'incap_ses_133_252730=fG04ESjt1WUqN78BhoTYAbkRj1QAAAAA5qT2zcgWTrHUm7QHHUJW9Q==; visid_incap_252730=Ycli/NoHTDm+u2IMeVZv47kRj1QAAAAAQUIPAAAAAADFEZZlHb2iH0YBkbQiJ1Rs; ___utmvc=navigator%3Dobject,navigator.vendor%3DGoogle%20Inc.,opera%3DReferenceError%3A%20opera%20is%20not%20defined,ActiveXObject%3DReferenceError%3A%20ActiveXObject%20is%20not%20defined,navigator.appName%3DNetscape,plugin%3Ddll,webkitURL%3Dfunction,navigator.plugins.length%3D%3D0%3Dfalse,digest=29481; ___utmvmOFukXyV=TtuwJrvaCnS; ___utmvbOFukXyV=IZQ XwfOyalG: jtY'
+		'Accept-Language': 'en-US,en;q=0.8'
 	}
 };
 var request = request.defaults(requestOptions);
@@ -25,7 +24,7 @@ module.exports.getHtml = function(url, callback) {
 
 module.exports.getRawHttp = function(url, callback) {
 	request.get(url, function(error, response, body) {
-		callback(error, response, body);
+		callback(error, response);
 	});
 };
 
