3bce9685305e5c369bb07ac3c028d7a8cd176f18
Adityo Jiwandono <adityo@traveloka.com>
2015-04-11 23:19:48 +0700

    Add event tracking.

--- public/page.js
+++ public/page.js
@@ -8,7 +8,9 @@ $(function() {
 
 	handleInputBoxChange();
 	detectRequestParameter();
+	initSubmitListener();
 	initRetryButton();
+	initDownloadButtonListener();
 	
 	/* ---------------- */
 	
@@ -34,30 +36,74 @@ $(function() {
 		var postData = {
 			url: url
 		};
-		
+
 		var endpoint = endpoints[Math.floor(Math.random() * endpoints.length)];
+
+		var t0 = 0;
+		try {
+			t0 = performance.now();
+		} catch(e) {
+			// no-op
+		}
 
 		$.ajax({
 			type: "POST",
 			url: endpoint + "/getDownloadables",
 			data: postData,
 			success: function(response) {
+				var elapsedMsec = 0;
+				try {
+					var t1 = performance.now();
+					elapsedMsec = Math.round(t1 - t0);
+				} catch(e) {
+					// no-op
+				}
+
 				var downloadables = response.downloadables;
 				if(downloadables.length > 0) {
+					ga('send', 'event', 'downloads', 'submit-success', url, elapsedMsec);
 					handleSuccess(downloadables);
 				} else {
+					ga('send', 'event', 'downloads', 'submit-fail-int', url, elapsedMsec);
 					handleFail();
 				}
 			},
 			error: function() {
+				try {
+					var t1 = performance.now();
+					var elapsedMsec = Math.round(t1 - t0);
+					ga('send', 'event', 'downloads', 'submit-fail-ext', url, elapsedMsec);
+				} catch(e) {
+					// no-op
+				}
+
 				handleFail();
 			}
+		});
+	}
+
+	function initSubmitListener() {
+		$('#form-main').submit(function(e) {
+			var origUrl = $('#form-text').val();
+			var trimUrl = origUrl.trim();
+			$('#form-text').val(trimUrl);
+			ga('send', 'event', 'downloads', 'submit', trimUrl);
 		});
 	}
 
 	function initRetryButton() {
 		$('a.button-retry').click(function() {
 			detectRequestParameter();
+		});
+	}
+
+	function initDownloadButtonListener() {
+		$(document).on('click', '.button-download', function(e) {
+			ga('send', 'event', 'downloads', 'download-lclick');
+		});
+
+		$(document).on('contextmenu', '.button-download', function(e) {
+			ga('send', 'event', 'downloads', 'download-rclick');
 		});
 	}
 	
